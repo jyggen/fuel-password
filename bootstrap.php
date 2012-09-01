da23410782acae5da80615573a5cb9899ab6e876
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-09-01 03:23:10 +0200

    Ported PHPass into a FuelPHP package.

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Password: Portable PHP password hashing framework (PHPass) as a FuelPHP package.
+ *
+ * @package		Password
+ * @version		1.0 (Based on PHPass 0.3)
+ * @author		Jonas Stendahl
+ * @license		MIT License
+ * @copyright	2012 Jonas Stendahl
+ * @link		https://github.com/jyggen/fuel-password
+ */
+
+Autoloader::add_core_namespace('Password');
+
+Autoloader::add_classes(array(
+	'Password\\Password' => __DIR__.DS.'classes'.DS.'password.php',
+));
